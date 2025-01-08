3825c5b04860072edcb168e1be1fd0b0eb68f88d
DanielTechs <{ID}+{username}@users.noreply.github.com>
2025-01-07 22:08:54 -0700

    Testing Deployment 2

--- db/seed.ts
+++ db/seed.ts
@@ -1,7 +1,7 @@
 import {PrismaClient} from '@prisma/client'
 import sampleData from '@/db/sample-data'
 
-async function main(){
+async function main(){ 
     const prisma = new PrismaClient()
 await prisma.product.deleteMany()
 await prisma.product.createMany({data: sampleData.products})
